4948f96913947a2aef95c9fbe4c801e9feb0c8da
Oleh Karpovych <oleg.karpovich.94@gmail.com>
2020-09-14 09:46:41 +0300

    developing cart page

---
+++ components/CartFooterComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="cart-footer">
+    <div class="elements-wrapper">
+      <div class="footer-continue-button-container">
+        <nuxt-link
+          class="footer-continue-button-container__continue-button"
+          to="/"
+          ><span>Continue shopping</span>
+        </nuxt-link>
+      </div>
+      <div class="footer-buy-button-container">
+        <nuxt-link class="footer-buy-button-container__buy-button" to="/"
+          ><span>Make purchase!</span>
+        </nuxt-link>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -7,60 +7,8 @@
       :total-cost="getTotalPrice"
       :mobile-content-scroll="mobileContentScrollComputed"
     />
-    <div class="cart-content">
-      <div class="cart-content-wrapper">
-        <div class="table-wrapper">
-          <table>
-            <thead>
-              <tr>
-                <td class="list-cell">Shop List</td>
-                <td class="amount-cell">Amount</td>
-                <td class="price-cell">Price</td>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(item, index) in getCartData" :key="index">
-                <td>
-                  <button
-                    class="delete-btn"
-                    @click="deleteCartItemsGroup(item)"
-                  ></button>
-                  <span>{{ item.name }}</span>
-                </td>
-                <td>
-                  <button class="minus-btn" @click="removeCartItem(item)">
-                    <span>-</span>
-                  </button>
-                  {{ item.amount }}
-                  <button class="plus-btn" @click="addAmount(item)">
-                    <span>+</span>
-                  </button>
-                </td>
-                <td>${{ item.amount * item.cost }}</td>
-              </tr>
-            </tbody>
-            <tfoot>
-              <tr>
-                <td>
-                  <button class="clear-all-btn" @click="clearAll()">
-                    Clear All
-                  </button>
-                </td>
-                <td></td>
-                <td>
-                  Sum
-                  <span class="final-cost">${{ getTotalPrice }}</span>
-                </td>
-              </tr>
-            </tfoot>
-          </table>
-        </div>
-        <div class="buying-btns">
-          <nuxt-link to="/" class="continue">Continue Shopping</nuxt-link>
-          <button @click="buyItems()">Buy</button>
-        </div>
-      </div>
-    </div>
+    <div class="cart-content"></div>
+    <CartFooterComponent />
     <EmptyFolderMessageComponent
       v-if="getTotalPrice === 0"
       message="Your cart is empty now!"
@@ -70,8 +18,12 @@
 
 <script>
 import { mapGetters, mapMutations } from 'vuex'
+import CartFooterComponent from '@/components/CartFooterComponent'
 export default {
   layout: 'mainLayout',
+  components: {
+    CartFooterComponent
+  },
   data() {
     return {
       mobileContentScroll: 0
