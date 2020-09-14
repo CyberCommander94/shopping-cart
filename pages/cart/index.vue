ed94fe92150f7a8ed63df8d047f35e751eb030ea
Oleh Karpovych <oleg.karpovich.94@gmail.com>
2020-09-14 11:48:47 +0300

    developing cart content

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -7,7 +7,35 @@
       :total-cost="getTotalPrice"
       :mobile-content-scroll="mobileContentScrollComputed"
     />
-    <div class="cart-content"></div>
+    <div class="cart-content">
+      <div class="cart-content__header">
+        <div class="name-cell">Name:</div>
+        <div class="amount-cell">Amount:</div>
+        <div class="delete-btn-cell"></div>
+      </div>
+      <div class="cart-content__main">
+        <div class="main-content-wrapper">
+          <div class="order-row">
+            <div class="name-cell">sdfsdfsdf jhhkh hkgghkh</div>
+            <div class="amount-cell">
+              <div class="amount-cell__controls">
+                <div class="div-amount">-</div>
+                <span class="total-amount">12</span>
+                <div class="add-amount">+</div>
+              </div>
+              <div class="amount-cell__total">$1200</div>
+            </div>
+            <div class="delete-btn-cell">
+              <div class="delete-btn-cell__delete-button"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="cart-content__footer">
+        <div>Cart total price:</div>
+        <div>$10000</div>
+      </div>
+    </div>
     <CartFooterComponent />
     <EmptyFolderMessageComponent
       v-if="getTotalPrice === 0"
